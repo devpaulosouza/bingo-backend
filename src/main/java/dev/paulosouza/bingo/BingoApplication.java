35225fda450acb811b34ef947393489c5ec11574
Paulo Souza <paulosouza.ti@gmail.com>
2023-12-19 13:20:46 -0300

    initial structure

---
+++ src/main/java/dev/paulosouza/bingo/BingoApplication.java
@@ -0,0 +1,13 @@
+package dev.paulosouza.bingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BingoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/paulosouza/bingo/BingoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.paulosouza.bingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
