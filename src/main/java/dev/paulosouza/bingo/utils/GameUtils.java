c15d84e67366b783db45edab7f7f9da99efdc568
Paulo Souza <paulosouza.ti@gmail.com>
2024-01-01 21:53:59 -0300

    fix: rollback blackout

--- src/main/java/dev/paulosouza/bingo/utils/GameUtils.java
+++ src/main/java/dev/paulosouza/bingo/utils/GameUtils.java
@@ -12,57 +12,48 @@ public class GameUtils {
 
     @SuppressWarnings("java:S3776")
     public static boolean checkWinner(boolean[][] markedNumbers, int[][] numbers, List<Integer> drawnNumbers) {
-        boolean winner = true;
+        boolean wonByMainDiagonal = true;
+        boolean wonBySecondaryDiagonal = true;
+
         for (int i = 0; i < markedNumbers.length; i++) {
+            boolean wonByRow = true;
+            boolean wonByCol = true;
+
             for (int j = 0; j < markedNumbers[0].length; j++) {
-                winner &= markedNumbers[i][j] && drawnNumbers.contains(numbers[i][j]);
+
+                if (i == 2 && j == 2) {
+                    continue;
+                }
+
+                wonByRow &= markedNumbers[i][j] && drawnNumbers.contains(numbers[i][j]);
+                wonByCol &= markedNumbers[j][i] && drawnNumbers.contains(numbers[j][i]);
+
+                if (i == j) {
+                    wonByMainDiagonal &= markedNumbers[i][j] && drawnNumbers.contains(numbers[i][j]);
+                }
+
+                if (i + j == 4) {
+                    wonBySecondaryDiagonal &= markedNumbers[i][j] && drawnNumbers.contains(numbers[i][j]);
+                }
+
+                if (j == numbers.length - 1 && wonByCol) {
+                    return true;
+                }
+
+                if (j == numbers.length - 1 && wonByRow) {
+                    return true;
+                }
+
+                if (
+                        (i == numbers.length - 1 && j == numbers.length - 1)
+                        && (wonByMainDiagonal || wonBySecondaryDiagonal)
+                ) {
+                    return true;
+                }
             }
         }
 
-        return winner;
-
-//        boolean wonByMainDiagonal = true;
-//        boolean wonBySecondaryDiagonal = true;
-//
-//        for (int i = 0; i < markedNumbers.length; i++) {
-//            boolean wonByRow = true;
-//            boolean wonByCol = true;
-//
-//            for (int j = 0; j < markedNumbers[0].length; j++) {
-//
-//                if (i == 2 && j == 2) {
-//                    continue;
-//                }
-//
-//                wonByRow &= markedNumbers[i][j] && drawnNumbers.contains(numbers[i][j]);
-//                wonByCol &= markedNumbers[j][i] && drawnNumbers.contains(numbers[j][i]);
-//
-//                if (i == j) {
-//                    wonByMainDiagonal &= markedNumbers[i][j] && drawnNumbers.contains(numbers[i][j]);
-//                }
-//
-//                if (i + j == 4) {
-//                    wonBySecondaryDiagonal &= markedNumbers[i][j] && drawnNumbers.contains(numbers[i][j]);
-//                }
-//
-//                if (j == numbers.length - 1 && wonByCol) {
-//                    return true;
-//                }
-//
-//                if (j == numbers.length - 1 && wonByRow) {
-//                    return true;
-//                }
-//
-//                if (
-//                        (i == numbers.length - 1 && j == numbers.length - 1)
-//                        && (wonByMainDiagonal || wonBySecondaryDiagonal)
-//                ) {
-//                    return true;
-//                }
-//            }
-//        }
-//
-//        return false;
+        return false;
     }
 
     public static int[][] drawCardNumbers() {
