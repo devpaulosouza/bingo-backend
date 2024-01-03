fc5997303566483526fbcf4e7cd153c1ecc9023d
Paulo Souza <paulosouza.ti@gmail.com>
2024-01-03 11:10:51 -0300

    fix(game): maximum 50 players

--- src/main/java/dev/paulosouza/bingo/game/GameService.java
+++ src/main/java/dev/paulosouza/bingo/game/GameService.java
@@ -314,7 +314,7 @@ public class GameService {
     }
 
     private void validateMaximumPlayers() {
-        if (this.cards.size() >= 100) {
+        if (this.cards.size() >= 50) {
             throw new UnprocessableEntityException("Max players reached");
         }
     }
