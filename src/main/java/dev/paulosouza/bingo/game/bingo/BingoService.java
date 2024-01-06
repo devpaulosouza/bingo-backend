91a66cea280f111ac6c79ccda2c3d32314ac2229
Paulo Souza <paulosouza.ti@gmail.com>
2024-01-05 21:45:27 -0300

    refactor(bingo): try 90 players

--- src/main/java/dev/paulosouza/bingo/game/bingo/BingoService.java
+++ src/main/java/dev/paulosouza/bingo/game/bingo/BingoService.java
@@ -384,7 +384,7 @@ public class BingoService {
     }
 
     private void validateMaximumPlayers() {
-        if (this.cards.size() >= 75) {
+        if (this.cards.size() >= 90) {
             throw new UnprocessableEntityException("Max players reached");
         }
     }
