318ad532fc2ce9a4081b6615cc9dda2d2cfb9907
Paulo Souza <paulosouza.ti@gmail.com>
2024-02-05 14:46:59 -0300

    fix(shuffle): timezone

--- src/main/java/dev/paulosouza/bingo/dto/shuffle/response/ShuffleGamePlayerResponse.java
+++ src/main/java/dev/paulosouza/bingo/dto/shuffle/response/ShuffleGamePlayerResponse.java
@@ -1,10 +1,6 @@
 package dev.paulosouza.bingo.dto.shuffle.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import dev.paulosouza.bingo.game.shuffle.ShufflePlayer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -36,9 +32,7 @@ public class ShuffleGamePlayerResponse {
 
     private int playersCount;
 
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssZ")
     private ZonedDateTime startAt;
 
     private boolean isStarted;
